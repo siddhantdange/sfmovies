9c155daa52a4e93cf178e85381621d59a03629f2
Sid Dange <siddhantdange@berkeley.edu>
2016-06-01 16:23:06 -0700

    feat(list movies): get a list of movies w/ filters

--- test/plugins/features/movie/controller.test.js
+++ test/plugins/features/movie/controller.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Controller = require('../../../../lib/plugins/features/movies/controller');
-const Movie      = require('../../../../lib/models/movie');
+const Knex       = require('../../../../lib/libraries/knex');
 
 describe('movie controller', () => {
 
@@ -13,12 +13,47 @@ describe('movie controller', () => {
       return Controller.create(payload)
       .then((movie) => {
         expect(movie.get('title')).to.eql(payload.title);
+      });
+    });
 
-        return new Movie({ id: movie.id }).fetch();
-      })
-      .then((movie) => {
-        expect(movie.get('title')).to.eql(payload.title);
-        expect(movie.get('release_year')).to.eql(payload.release_year);
+  });
+
+  describe('list', () => {
+
+    const movie_one = { title: 'foo', release_year: 2016 };
+    const movie_two = { title: 'bar', release_year: 2016 };
+    const movie_three = { title: 'foo', release_year: 2015 };
+
+    beforeEach(() => {
+      return Knex('movies').insert([movie_one, movie_two, movie_three]);
+    });
+
+    it('lists all movies', () => {
+      const query = {};
+
+      return Controller.list(query)
+      .then((movies) => {
+        expect(movies).to.have.length(3);
+      });
+    });
+
+    it('lists all movies from year', () => {
+      const query = { release_year: movie_one.release_year };
+
+      return Controller.list(query)
+      .then((movies) => {
+        expect(movies).to.have.length(2);
+        expect(movies.models[0].get('release_year')).to.be.equal(movie_one.release_year);
+      });
+    });
+
+    it('lists all movies from title', () => {
+      const query = { title: movie_two.title };
+
+      return Controller.list(query)
+      .then((movies) => {
+        expect(movies).to.have.length(1);
+        expect(movies.models[0].get('title')).to.be.equal(movie_two.title);
       });
     });
 
--- test/plugins/features/movie/index.test.js
+++ test/plugins/features/movie/index.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Movies = require('../../../../lib/server');
+const Knex   = require('../../../../lib/libraries/knex');
 
 describe('movies integration', () => {
 
@@ -15,6 +16,30 @@ describe('movies integration', () => {
       .then((response) => {
         expect(response.statusCode).to.eql(200);
         expect(response.result.object).to.eql('movie');
+      });
+    });
+
+  });
+
+  describe('list', () => {
+
+    const movie = { title: 'foo', release_year: 2016 };
+
+    beforeEach(() => {
+      return Knex('movies').insert(movie);
+    });
+
+    it('lists all movie', () => {
+      return Movies.inject({
+        url: '/movies',
+        method: 'GET',
+        payload: {}
+      })
+      .then((response) => {
+        expect(response.statusCode).to.eql(200);
+        expect(response.result.length).to.be.above(0);
+        expect(response.result[0].title).to.be.equal(movie.title);
+        expect(response.result[0].release_year).to.be.equal(movie.release_year);
       });
     });
 
